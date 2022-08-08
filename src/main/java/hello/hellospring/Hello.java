9d2c6792635d2630c989d9d3200747d0739fefbc
tom <tom@axisj.com>
2022-08-08 14:02:24 +0900

    Initial commit

---
+++ src/main/java/hello/hellospring/Hello.java
@@ -0,0 +1,13 @@
+package hello.hellospring;
+
+public class Hello {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,38 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.Hello;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "SORA SORA!!");
+        return "good";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(name = "name", defaultValue = "TOM") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam(name = "name", defaultValue = "TOM") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+}
